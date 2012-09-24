682088ce6079d6371b57d8b554c91f6c453930f2
Andrew Frank-Loron <frankloron@gmail.com>
2012-09-24 01:22:18 -0500

    Added parent - racer relationship

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,3 +1,10 @@
 class Parent < ActiveRecord::Base
   attr_accessible :firstname, :lastname, :phone1, :phone2
+
+  has_many :racers
+
+  validates :firstname, :length => { :maximum => 45, :minimum => 2 }
+  validates :lastname, :length => { :maximum => 45, :minimum => 2 }
+  validates :phone1, :length => { :maximum => 12, :minimum => 7 }
+
 end
--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,3 +1,6 @@
 class Racer < ActiveRecord::Base
   attr_accessible :city, :dob, :email, :emergencyphone, :firstname, :gender, :lastname, :phone1, :phone2, :racerlevel, :state, :street1, :street2, :zipcode
+
+  belongs_to :parent
+
 end
